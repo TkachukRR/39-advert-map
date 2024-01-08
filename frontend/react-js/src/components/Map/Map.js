d59041fed76f6e4b186b307a35d7436ce92dc2b0
Roman Tkachuk <tkachukrr@gmail.com>
2024-01-08 14:56:40 +0100

    [react] refactored default map state moved to settings.js

--- frontend/react-js/src/components/Map/Map.js
+++ frontend/react-js/src/components/Map/Map.js
@@ -1,10 +1,11 @@
 import './Map.css';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer } from 'react-leaflet';
+import { mapSettings } from '../../settings';
 
 export default function Map() {
   return (
-    <MapContainer center={[50.5, 30.5]} zoom={13}>
+    <MapContainer center={mapSettings.center} zoom={mapSettings.zoom}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
---
+++ frontend/react-js/src/settings.js
@@ -0,0 +1,4 @@
+export const mapSettings = {
+  center: [50.4501, 30.5234], // Kyiv UA
+  zoom: 15,
+};
