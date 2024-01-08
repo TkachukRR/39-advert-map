77b6cffe31147a032faacaf51e654f00bbd735ba
Roman Tkachuk <tkachukrr@gmail.com>
2024-01-08 14:52:19 +0100

    [react] init map with default settings

--- frontend/react-js/src/App.js
+++ frontend/react-js/src/App.js
@@ -2,9 +2,5 @@ import './App.css';
 import Map from './components/Map/Map';
 
 export default function App() {
-  return (
-    <div>
-      <Map />
-    </div>
-  );
+  return <Map />;
 }
--- frontend/react-js/src/components/Map/Map.js
+++ frontend/react-js/src/components/Map/Map.js
@@ -1,5 +1,14 @@
 import './Map.css';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 export default function Map() {
-  return <div>Map</div>;
+  return (
+    <MapContainer center={[50.5, 30.5]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
 }
