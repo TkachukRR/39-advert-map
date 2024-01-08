9c23455cdbf23b7392cc5b919903f0e52ec11896
Roman Tkachuk <tkachukrr@gmail.com>
2024-01-08 20:12:32 +0100

    [react] implemented ability to get mocked markers in App component

--- frontend/react-js/src/App.js
+++ frontend/react-js/src/App.js
@@ -1,6 +1,28 @@
 import './App.css';
 import Map from './components/Map/Map';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    const fetchMarkers = async () => {
+      try {
+        const response = await fetch('/mockedMarkers.json');
+        const markersData = await response.json();
+
+        if (!response.ok) {
+          throw new Error('Loading markers error');
+        }
+
+        setMarkers(markersData);
+      } catch (error) {
+        console.error('Loading markers error: ', error);
+      }
+    };
+
+    fetchMarkers();
+  }, []);
+
   return <Map />;
 }
