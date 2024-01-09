4905610327d963cdcb9a99b57a475dd6f0cdf865
Roman Tkachuk <tkachukrr@gmail.com>
2024-01-09 12:27:42 +0100

    [react] added ability close/open AddForm component

--- frontend/react-js/src/components/List/List.js
+++ frontend/react-js/src/components/List/List.js
@@ -1,9 +1,20 @@
 import classes from './List.module.css';
 import Advert from '../Advert/Advert';
+import { useState } from 'react';
+import AddForm from '../AddForm/AddForm';
 
 export default function List({ list, title, activeAdvert }) {
+  const [isAddFormOpen, setIsAddFormOpen] = useState(false);
+
+  const toggleAddFormVisibility = () =>
+    setIsAddFormOpen((prevState) => !prevState);
+
   return (
     <div className={classes.list__wrapper}>
+      <button className={classes.list__btn} onClick={toggleAddFormVisibility}>
+        {isAddFormOpen ? '-' : '+'}
+      </button>
+      {isAddFormOpen && <AddForm />}
       <h3 className={classes.list__title}>{title}</h3>
       <ul className={classes.list}>
         {activeAdvert && (
